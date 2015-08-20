3d715cd2684105097b65160e36897c12ff283237
Scott Williams <williamsjs1224@gmail.com>
2015-08-20 17:26:06 -0400

    add rspec spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814161804) do
+ActiveRecord::Schema.define(version: 20150820164353) do
 
   create_table "mains", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "phrases", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
